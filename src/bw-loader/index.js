8d77d1391ad1e3475afe903cce40a19b4c45cabe
Jeremy Gonzalez <jeremy@7geese.com>
2018-05-01 07:27:16 -0700

    add base loader

---
+++ src/bw-loader/index.js
@@ -0,0 +1,8 @@
+export class Loader {
+    findView() {
+        throw Error('findView needs to be defined');
+    }
+    load(moduleId) {
+        throw Error('load needs to be defined');
+    }
+}
